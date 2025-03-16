ef5c30ed61434c1beb0576944973b302c4620e29
Aneesha Bhattacharjee <aneeshab@andrew.cmu.edu>
2025-03-16 18:49:18 -0400

    testing bluetooth from laptop to tell the servos to move

--- pillPopperPro/server.py
+++ pillPopperPro/server.py
@@ -1,16 +1,11 @@
 # source to see if bluetooth is working: https://www.youtube.com/watch?v=8pMaR-WUc6U
 
 import socket
-# import RPi.GPIO as GPIO
-# import time
 
 SERVER_ADDRESS = "2C:CF:67:7E:B0:E4"  # Replace with Raspberry Pi's Bluetooth MAC
 PORT = 1
 
-# GPIO.setmode(GPIO.BOARD)
-# GPIO.setup(18, GPIO.OUT) 
-# pwm = GPIO.PWM(18, 50)  
-# pwm.start(0)  
+ 
 
 
 server = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
@@ -19,13 +14,7 @@ server.listen(PORT)
 
 client, addr = server.accept()
 
-# def set_servo_position(angle):
-#     duty = (angle / 18) + 2 
-#     GPIO.output(18, True)
-#     pwm.ChangeDutyCycle(duty)
-#     time.sleep(1)
-#     GPIO.output(18, False)
-#     pwm.ChangeDutyCycle(0)
+
 
 try:
     while True:
@@ -41,23 +30,3 @@ except OSError as e:
 client.close()
 server.close()
 
-# try:
-#     while True:
-#         data = client.recv(1024)
-#         if not data:
-#             break
-#         message = data.decode('utf-8')
-#         print(f"Received message: {message}")
-
-#         if message.lower() == "dispense":
-#             set_servo_position(180)  
-#             client.send(b"Dispensing... Servo moved to 180 degrees.")
-#         else:
-#             client.send(b"Invalid command. Please send 'dispense'.")
-        
-# except OSError as e:
-#     pass
-
-# client.close()
-# server.close()
-# GPIO.cleanup()
