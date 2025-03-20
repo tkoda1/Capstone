387e3c3dc76713e383eb01b728a700bf1755b708
Aneesha Bhattacharjee <aneeshab@andrew.cmu.edu>
2025-03-20 18:39:50 -0400

    Converting from gpiozero to using the i2c connection

--- pillPopperPro/serverservo.py
+++ pillPopperPro/serverservo.py
@@ -1,23 +1,79 @@
 # Referencing GPIO documentation at https://gpiozero.readthedocs.io/en/v1.5.1/recipes.html#servo
 
+# import socket
+# from gpiozero import AngularServo
+# from time import sleep
+
+# SERVER_ADDRESS = "2C:CF:67:7E:B0:E4"  
+# PORT = 1
+
+# servo = AngularServo(18, min_angle=0, max_angle=180, min_pulse_width=0.5/1000, max_pulse_width=2.5/1000)
+
+# def set_angle(angle):
+#     servo.angle = angle
+#     sleep(1)
+
+# server = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+# server.bind((SERVER_ADDRESS, PORT))
+# server.listen(1)
+
+# print(f"Server listening on {SERVER_ADDRESS}:{PORT}...")
+
+# client, addr = server.accept()
+# print(f"Connected to {addr}")
+
+# try:
+#     while True:
+#         data = client.recv(1024)
+#         if not data:
+#             break
+#         message = data.decode('utf-8')
+        
+#         try:
+#             angle = int(message)
+#             if 0 <= angle <= 180:
+#                 print(f"Setting angle to: {angle}")
+#                 set_angle(angle)  
+#                 client.send(f"Angle set to {angle}".encode('utf-8'))
+#             else:
+#                 client.send("Please enter a valid angle between 0 and 180.".encode('utf-8'))
+#         except ValueError:
+#             client.send("Invalid input. Please send an integer.".encode('utf-8'))
+# except OSError as e:
+#     pass
+
+# client.close()
+# server.close()
+
 import socket
-from gpiozero import AngularServo
-from time import sleep
+import time
+from adafruit_pca9685 import PCA9685
+from board import SCL, SDA
+import busio
+
+i2c = busio.I2C(SCL, SDA)
+pca = PCA9685(i2c)
+pca.frequency = 50  
+
+SERVO_CHANNEL = 0  
+
+def set_angle(angle):
+    """Convert angle (0-180) to PCA9685 PWM signal and move the servo."""
+    min_pulse = 150  
+    max_pulse = 600 
+
+    pulse_length = min_pulse + (angle / 180) * (max_pulse - min_pulse)
+    pca.channels[SERVO_CHANNEL].duty_cycle = int(pulse_length)
+    time.sleep(1)  
 
 SERVER_ADDRESS = "2C:CF:67:7E:B0:E4"  
 PORT = 1
-
-servo = AngularServo(12, min_angle=0, max_angle=180, min_pulse_width=0.5/1000, max_pulse_width=2.5/1000)
-
-def set_angle(angle):
-    servo.angle = angle
-    sleep(1)
 
 server = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
 server.bind((SERVER_ADDRESS, PORT))
 server.listen(1)
 
-print(f"Server listening on {SERVER_ADDRESS}:{PORT}...")
+print(f"Bluetooth Server listening on {SERVER_ADDRESS}:{PORT}...")
 
 client, addr = server.accept()
 print(f"Connected to {addr}")
@@ -28,12 +84,12 @@ try:
         if not data:
             break
         message = data.decode('utf-8')
-        
+
         try:
             angle = int(message)
             if 0 <= angle <= 180:
-                print(f"Setting angle to: {angle}")
-                set_angle(angle)  
+                print(f"Received angle: {angle}, moving servo.")
+                set_angle(angle) 
                 client.send(f"Angle set to {angle}".encode('utf-8'))
             else:
                 client.send("Please enter a valid angle between 0 and 180.".encode('utf-8'))
@@ -44,3 +100,4 @@ except OSError as e:
 
 client.close()
 server.close()
+
