e324a2216d593941be9b72cf8507f1b0a3f276b4
Aneesha Bhattacharjee <aneeshab@andrew.cmu.edu>
2025-03-25 22:33:35 -0400

    Trying to reset the servo angle

--- pillPopperPro/serverservo.py
+++ pillPopperPro/serverservo.py
@@ -67,7 +67,16 @@ def dispense_pill(slot, angle=180):
     
     duty_cycle = int((pulse_width / period) * 65535)
 
-    pca.channels[slot].duty_cycle = 4096 
+    pca.channels[slot].duty_cycle = 2048
+
+    time.sleep(0.5)
+
+    pca.channels[slot].duty_cycle = 2048
+
+    time.sleep(0.5)
+
+
+
 
 SERVER_ADDRESS = "2C:CF:67:7E:B0:E4"  
 PORT = 1
