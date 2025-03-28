758a964768d09bdb580f4ea64adeeb0ba75bf535
MM Demangone <mdemango@andrew.cmu.edu>
2025-03-27 21:55:45 -0400

    example of how to store audio file

--- machine/constants.py
+++ machine/constants.py
@@ -1,5 +1,5 @@
 WEBSOCKET_URL = "ws://18.223.133.87/pillPopperPro/data/"
-RELEASE_PILL_PATH = "Sample.wav"
+RELEASE_PILL_PATH = "audioAlerts/testAudio.wav"
 REFILL_PILL_PATH = "Sample.wav"
 FINISH_DISPENSING_PATH = "Sample.wav"
 INVALID_DISPENSING_PATH = "Sample.wav"
