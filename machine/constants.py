217eef73a626040df87b32a8da879e013f2504d5
MM Demangone <mdemango@andrew.cmu.edu>
2025-03-30 16:19:49 -0400

    changed the websocket url

--- machine/constants.py
+++ machine/constants.py
@@ -1,4 +1,7 @@
-WEBSOCKET_URL = "ws://18.223.133.87/pillPopperPro/data/"
+
+
+# WEBSOCKET_URL = "ws://18.223.133.87/pillPopperPro/data/"
+WEBSOCKET_URL = "ws://pillpopperpro.com/pillPopperPro/data/"
 RELEASE_PILL_PATH = "audioAlerts/testAudio.wav"
 REFILL_PILL_PATH = "Sample.wav"
 FINISH_DISPENSING_PATH = "Sample.wav"
