f3b92a47d74bf20d13af9286645db18ee89a4c63
MM Demangone <mdemango@andrew.cmu.edu>
2025-04-07 12:11:20 -0400

    updated loadcell testing code

--- machine/loadcell.py
+++ machine/loadcell.py
@@ -1,34 +1,53 @@
-# https://chatgpt.com/share/67dca560-2a58-8002-8556-78d4938bd12b
+# https://pypi.org/project/hx711/
 
-import RPi.GPIO as GPIO
 from hx711 import HX711
-import time
 
-DOUT = 5
-SCK = 6 
+try:
+    hx711 = HX711(
+        dout_pin=5,
+        pd_sck_pin=6,
+        channel='A',
+        gain=64
+    )
 
-hx = HX711(DOUT, SCK)
+    hx711.reset()   # Before we start, reset the HX711 (not obligate)
+    measures = hx711.get_raw_data(num_measures=3)
+finally:
+    GPIO.cleanup()  # always do a GPIO cleanup in your scripts!
 
-calibration_factor = 2280  # You will need to calibrate your load cell and adjust this value
+print("\n".join(measures))
 
-def setup():
-    GPIO.setmode(GPIO.BCM)
-    GPIO.setwarnings(False)
-    hx.set_reading_format("MSB", "MSB")
-    hx.set_reference_unit(calibration_factor)
-    hx.reset()
-    hx.tare()
+# # https://chatgpt.com/share/67dca560-2a58-8002-8556-78d4938bd12b
 
-def read_weight():
-    weight = hx.get_weight(5)
-    return weight
+# import RPi.GPIO as GPIO
+# from hx711 import HX711
+# import time
 
-if __name__ == "__main__":
-    setup()
-    try:
-        while True:
-            weight = read_weight()
-            print(f"Weight: {weight} grams")
-            time.sleep(1)
-    finally:
-        GPIO.cleanup()
+# DOUT = 5
+# SCK = 6 
+
+# hx = HX711(DOUT, SCK)
+
+# calibration_factor = 2280  # You will need to calibrate your load cell and adjust this value
+
+# def setup():
+#     GPIO.setmode(GPIO.BCM)
+#     GPIO.setwarnings(False)
+#     hx.set_reading_format("MSB", "MSB")
+#     hx.set_reference_unit(calibration_factor)
+#     hx.reset()
+#     hx.tare()
+
+# def read_weight():
+#     weight = hx.get_weight(5)
+#     return weight
+
+# if __name__ == "__main__":
+#     setup()
+#     print('Testing load cell...')
+#     weight = read_weight()
+#     print('Read weight {weight} grams')
+#     time.sleep(1)
+#     GPIO.cleanup()
+#     time.sleep(1)
+#     print('Finished testing load cell')
