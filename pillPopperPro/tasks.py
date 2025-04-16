d1f2b3fdd0e29a9ab425cf2022b7cf57221a6608
MM Demangone <mdemango@andrew.cmu.edu>
2025-04-16 10:10:13 -0400

    fixed dispensing at midnight bug

--- pillPopperPro/tasks.py
+++ pillPopperPro/tasks.py
@@ -19,17 +19,18 @@ def reset_taken_today():
     print('Running reset taken today')
     for userprofile in UserProfile.objects.all():
         user = userprofile.user
-        user_tz = ZoneInfo(userprofile.timezone)
-        local_time = now_utc.astimezone(user_tz)
 
-        if local_time.hour == 23 and local_time.minute == 58:
-            caretakers = userprofile.caretakers.all()
-            pills = Pill.objects.filter(user=user)
-            pills_not_taken = []
-            print(user.username)
+        caretakers = userprofile.caretakers.all()
+        pills = Pill.objects.filter(user=user)
+        pills_not_taken = []
+        print(user.username)
 
-            # find which pills the user hasn't taken today, and reset any if taken
-            for pill in pills:
+        # find which pills the user hasn't taken today, and reset any if taken
+        for pill in pills:
+            pill_tz = ZoneInfo(pill.timezone)
+            local_time = now_utc.astimezone(pill_tz)
+
+            if (local_time.hour == 23) and (local_time.minute >= 58):
                 if pill.taken_today == 0:
                     pills_not_taken.append(pill.name)
                     print(f'User has not taken pill {pill.name}')
@@ -38,21 +39,20 @@ def reset_taken_today():
                     pill.save()
                     print(f'User has taken pill {pill}')
 
-            print(pills_not_taken)
-            # send email to user and caregivers if did not take all medication
-            if pills_not_taken != []:
-                print(caretakers)
-                user_subject = 'Alert: failure to take medication'
-                user_body = f"""Hello {user.get_full_name()},\n
-                        You have not taken the following medications
-                        on {date.today()}: {', '.join(pills_not_taken)}"""
-                user_email = user.email
-                send_email(user_subject, user_body, user_email)
-                caretaker_subject = 'Alert: patient failure to take medication'
-                for caretaker in caretakers:
-                    print(caretaker)
-                    # caretaker_body = f"""Hello {caretaker.get_full_name()},\n
-                    #     You have not taken the following medications
-                    #     on {date.today()}: {', '.join(pills_not_taken)}"""
-                    # caretaker_email = caretaker.email
-                    # send_email(caretaker_subject, caretaker_body, caretaker_email)
+        # send email to user and caregivers if did not take all medication
+        if pills_not_taken != []:
+            print(caretakers)
+            user_subject = 'Alert: failure to take medication'
+            user_body = f"""Hello {user.get_full_name()},\n
+                    You have not taken the following medications
+                    on {date.today()}: {', '.join(pills_not_taken)}"""
+            user_email = user.email
+            send_email(user_subject, user_body, user_email)
+            caretaker_subject = 'Alert: patient failure to take medication'
+            for caretaker in caretakers:
+                print(caretaker)
+                # caretaker_body = f"""Hello {caretaker.get_full_name()},\n
+                #     You have not taken the following medications
+                #     on {date.today()}: {', '.join(pills_not_taken)}"""
+                # caretaker_email = caretaker.email
+                # send_email(caretaker_subject, caretaker_body, caretaker_email)
