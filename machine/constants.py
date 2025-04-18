068d83e2c111a9263b44e0677572bc79f6866e7e
Aneesha Bhattacharjee <aneeshab@andrew.cmu.edu>
2025-04-17 20:29:58 -0400

    HTTPS connection for websocket

--- machine/constants.py
+++ machine/constants.py
@@ -1,7 +1,7 @@
 
 
 # WEBSOCKET_URL = "ws://18.223.133.87/pillPopperPro/data/"
-WEBSOCKET_URL = "ws://pillpopperpro.com/pillPopperPro/data/"
+WEBSOCKET_URL = "wss://pillpopperpro.com/pillPopperPro/data/"
 RELEASE_PILL_PATH = "audioAlerts/release_pill.wav"
 REFILL_PILL_PATH = "audioAlerts/refill.wav"
 FINISH_DISPENSING_PATH = "audioAlerts/dispensed.wav"
